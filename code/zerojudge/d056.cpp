ab0e172e22bc5bddddf414241ea75bd23d71ded7
HenryTang <henrytang0403@gmail.com>
2026-02-02 02:58:25 +0800

    d056

---
+++ code/zerojudge/d056.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int y;
+    cin>>y;
+    if(y%400==0)cout<<"a leap year";
+    else if(y%100==0)cout<<"a normal year";
+    else if(y%4==0)cout<<"a leap year";
+    else cout<<"a normal year";
+}
