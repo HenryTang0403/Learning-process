c5a519cc3d87d2e8c19cdfade302bd5cd662f501
HenryTang <henrytang0403@gmail.com>
2026-02-28 22:46:53 +0800

    d189

---
+++ code/zerojudge/d189.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    while(cin>>n){
+        int ans=n;
+        while(n>=3){
+            ans+=n/3;
+            n=n/3+n%3;
+        }
+        if(n==2)ans+=1;
+        cout<<ans<<"\n";
+    }
+}
